33fef6998654aab99bcc8f03a8d2cf02ebc59330
Rajesh Iyer <iyerajesh@gmail.com>
2019-11-15 17:22:02 -0500

    Integrated cloud events on the kafka consumer

--- orders-domain/docker/docker-build-push.sh
+++ orders-domain/docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f docker/Dockerfile --no-cache -t gcr.io/xylia-platform/orders-domain:2.0.11 .
-docker push gcr.io/xylia-platform/orders-domain:2.0.11
+docker build -f docker/Dockerfile --no-cache -t gcr.io/xylia-platform/orders-domain:2.0.13 .
+docker push gcr.io/xylia-platform/orders-domain:2.0.13
 
 # docker run --name orders-domain -d gcr.io/xylia-platform/orders-domain:latest
--- orders-domain/src/main/java/com/xylia/domain/orders/consumer/KafkaConsumer.java
+++ orders-domain/src/main/java/com/xylia/domain/orders/consumer/KafkaConsumer.java
@@ -1,7 +1,9 @@
 package com.xylia.domain.orders.consumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xylia.domain.orders.model.CustomerOrders;
 import com.xylia.domain.orders.repository.CustomerOrdersRepository;
+import io.cloudevents.v03.CloudEventImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,16 +18,22 @@ public class KafkaConsumer {
     @Autowired
     private CustomerOrdersRepository customerOrdersRepository;
     private CountDownLatch latch = new CountDownLatch(1);
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public CountDownLatch getLatch() {
         return latch;
     }
 
     @KafkaListener(topics = "${spring.kafka.topic.accounts-customer}")
-    public void receiveCustomerOrder(CustomerOrders customerOrders) {
+    public void receiveCustomerOrder(CustomerOrders customerOrders) throws Exception {
 
-        log.info("received payload='{}'", customerOrders);
-//        latch.countDown();
+//        log.info("received cloud event payload='{}'", cloudEvent);
+//
+//        CustomerOrders customerOrders = objectMapper
+//                .readValue(cloudEvent.getData().get(), CustomerOrders.class);
+
+        log.info("customer order payload='{}'", customerOrders);
+
         customerOrdersRepository.save(customerOrders);
     }
 
