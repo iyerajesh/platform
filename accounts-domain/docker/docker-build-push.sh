b5bfcbb825b6cccff5a8549aec51ff721cabc405
Rajesh Iyer <iyerajesh@gmail.com>
2019-11-21 17:53:02 -0500

    Upgraded to 2.0.14

--- accounts-domain/docker/docker-build-push.sh
+++ accounts-domain/docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f docker/Dockerfile --no-cache -t gcr.io/xylia-platform/accounts-domain:2.0.13 .
-docker push gcr.io/xylia-platform/accounts-domain:2.0.13
+docker build -f docker/Dockerfile --no-cache -t gcr.io/xylia-platform/accounts-domain:2.0.14 .
+docker push gcr.io/xylia-platform/accounts-domain:2.0.14
 
 # docker run --name accounts-domain -d gcr.io/xylia-platform/accounts-domain:latest
