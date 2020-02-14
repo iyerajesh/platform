3765160258a6bb44da9417894c07225e20f810da
Rajesh Iyer <iyerajesh@gmail.com>
2020-02-14 12:20:08 -0500

    Ugraded the versions to 2.0.21

--- accounts-domain/docker/docker-build-push.sh
+++ accounts-domain/docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f docker/Dockerfile --no-cache -t gcr.io/xylia-platform/accounts-domain:2.0.20 .
-docker push gcr.io/xylia-platform/accounts-domain:2.0.20
+docker build -f docker/Dockerfile --no-cache -t gcr.io/xylia-platform/accounts-domain:2.0.21 .
+docker push gcr.io/xylia-platform/accounts-domain:2.0.21
 
 # docker run --name accounts-domain -d gcr.io/xylia-platform/accounts-domain:latest
